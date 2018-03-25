5e311180566dd972955cb765c58a980ff0052cc4
anah_ <anah_@LAPTOP-6NB3I825>
2018-03-25 18:37:29 -0300

    Adicionando classe Endereco

--- SistemaEntregas/Modelos/Pessoa.cs
+++ SistemaEntregas/Modelos/Pessoa.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Modelos
+﻿namespace Modelos
 {
     public class Pessoa
     {
         public int PessoaID { get; set; }
 
         public string Nome { get; set; }
-
 
         public string Cpf { get; set; }
     }
