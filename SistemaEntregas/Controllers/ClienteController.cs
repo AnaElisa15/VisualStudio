5aac5243e4e97751ab7e49029bd788c7edb231f6
anah_ <anah_@LAPTOP-6NB3I825>
2018-03-25 23:32:51 -0300

    Adicionando metodo SalvarCliente e criando e instanciando a lista Clientes

--- SistemaEntregas/Controllers/ClienteController.cs
+++ SistemaEntregas/Controllers/ClienteController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelos;
 
 namespace Controllers
 {
     public class ClienteController
     {
+        public List<Cliente> MeusClientes { get; set; }
+
+        public ClienteController()
+        {
+            MeusClientes = new List<Cliente>();
+        }
+
+
+        public void SalvarCliente(Cliente cliente)
+        {
+            MeusClientes.Add(cliente);
+        }
       
     }
 }
